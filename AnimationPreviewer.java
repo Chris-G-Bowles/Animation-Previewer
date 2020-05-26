3a70ed949cb2fc354c04218011ae13fed751379b
Chris-G-Bowles <chris.g.bowles@gmail.com>
2020-05-25 21:57:39 -0700

    Completely reworked the program error-checking flow.
    The program now works once again.

--- AnimationPreviewer.java
+++ AnimationPreviewer.java
@@ -31,6 +31,10 @@ public class AnimationPreviewer {
 		} else {
 			directoryLocation = args[0];
 		}
+		File directory = new File(directoryLocation);
+		if (!directory.isDirectory()) {
+			error(directoryLocation + " is not a valid directory.");
+		}
 		String patternResolutionString;
 		if (args.length == 0) {
 			System.out.println("Select the pattern resolution to use:");
@@ -43,6 +47,16 @@ public class AnimationPreviewer {
 		} else {
 			patternResolutionString = args[1];
 		}
+		Scanner lineInput1 = new Scanner(patternResolutionString);
+		if (!lineInput1.hasNextInt()) {
+			error("Invalid pattern resolution input.");
+		}
+		int patternResolutionOption = lineInput1.nextInt();
+		if (patternResolutionOption < 1 || patternResolutionOption > 4) {
+			error("Invalid pattern resolution option.");
+		}
+		lineInput1.close();
+		patternLength = (int)Math.pow(2, patternResolutionOption + 2);
 		String paletteSizeString;
 		if (args.length == 0) {
 			System.out.println("Select the palette size to use:");
@@ -54,66 +68,60 @@ public class AnimationPreviewer {
 		} else {
 			paletteSizeString = args[2];
 		}
-		input.close();
-		File directory = new File(directoryLocation);
-		if (directory.isDirectory()) {
-			if (isValidInteger(patternResolutionString) && Integer.parseInt(patternResolutionString) >= 1 &&
-					Integer.parseInt(patternResolutionString) <= 4) {
-				patternLength = (int)Math.pow(2, Integer.parseInt(patternResolutionString) + 2);
-				if (isValidInteger(paletteSizeString) && Integer.parseInt(paletteSizeString) >= 1 &&
-						Integer.parseInt(paletteSizeString) <= 3) {
-					int paletteSize = (Integer.parseInt(paletteSizeString) * 2) + 2;
-					grayscaleAmount = 255 / ((Integer.parseInt(paletteSizeString) * 2) + 1);
-					System.out.println("(Please wait a few seconds for the patterns to load.)");
-					addPatternsFromDirectory(directory);
-					if (patterns.size() >= MINIMUM_LIST_SIZE) {
-						JFrame frame = new JFrame("Animation Previewer");
-						AnimationPreviewerPanel panel = new AnimationPreviewerPanel(frame, directoryLocation,
-								patterns, patternLength, paletteSize, grayscaleAmount);
-						frame.setContentPane(panel);
-						int frameWidth = 1280;
-						int frameHeight = 720;
-						if (OPERATING_SYSTEM.indexOf("mac") >= 0) {
-							frameHeight += 22;
-						} else if (OPERATING_SYSTEM.indexOf("win") >= 0) {
-							frameWidth += 16;
-							frameHeight += 39;
-						}
-						frame.setSize(frameWidth, frameHeight);
-						frame.setLocationRelativeTo(null);
-						frame.setResizable(false);
-						frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-						frame.setVisible(true);
-					} else {
-						System.out.println("Error: " + directoryLocation + " contains less than " +
-								MINIMUM_LIST_SIZE + " pattern.");
-					}
-				} else {
-					System.out.println("Error: Invalid palette size option.");
-				}
-			} else {
-				System.out.println("Error: Invalid pattern resolution option.");
-			}
-		} else {
-			System.out.println("Error: " + directoryLocation + " is not a valid directory.");
+		Scanner lineInput2 = new Scanner(paletteSizeString);
+		if (!lineInput2.hasNextInt()) {
+			error("Invalid palette size input.");
 		}
+		int paletteSizeOption = lineInput2.nextInt();
+		if (paletteSizeOption < 1 || paletteSizeOption > 3) {
+			error("Invalid palette size option.");
+		}
+		lineInput2.close();
+		int paletteSize = (paletteSizeOption * 2) + 2;
+		grayscaleAmount = 255 / ((paletteSizeOption * 2) + 1);
+		input.close();
+		System.out.println("(Please wait a few seconds for the patterns to load.)");
+		addPatternsFromDirectory(directory);
+		if (patterns.size() < MINIMUM_LIST_SIZE) {
+			error(directoryLocation + " contains less than " + MINIMUM_LIST_SIZE + " pattern.");
+		}
+		JFrame frame = new JFrame("Animation Previewer");
+		AnimationPreviewerPanel panel = new AnimationPreviewerPanel(frame, directoryLocation, patterns, patternLength,
+				paletteSize, grayscaleAmount);
+		frame.setContentPane(panel);
+		int frameWidth = 1280;
+		int frameHeight = 720;
+		if (OPERATING_SYSTEM.indexOf("mac") >= 0) {
+			frameHeight += 22;
+		} else if (OPERATING_SYSTEM.indexOf("win") >= 0) {
+			frameWidth += 16;
+			frameHeight += 39;
+		}
+		frame.setSize(frameWidth, frameHeight);
+		frame.setLocationRelativeTo(null);
+		frame.setResizable(false);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setVisible(true);
 	}
 	
 	private static void addPatternsFromDirectory(File directory) {
-		File[] files = directory.listFiles();
-		for (int i = 0; i < files.length && patterns.size() < MAXIMUM_LIST_SIZE; i++) {
-			if (files[i].isFile()) {
-				addPatternFromFile(files[i]);
-			} else if (files[i].isDirectory()) {
-				addPatternsFromDirectory(files[i]);
-			}
-		}
-	}
-	
-	private static void addPatternFromFile(File file) {
-		try {
-			BufferedImage pattern = ImageIO.read(file);
-			if (pattern.getWidth() == patternLength && pattern.getHeight() == patternLength) {
+		for (File file : directory.listFiles()) {
+			if (file.isFile()) {
+				BufferedImage pattern;
+				try {
+					pattern = ImageIO.read(file);
+				} catch (Exception e) {
+					pattern = null;
+				}
+				if (pattern == null) {
+					System.out.println(file.getPath() + " does not contain a readable pattern, and is being skipped.");
+					return;
+				}
+				if (pattern.getWidth() != patternLength || pattern.getHeight() != patternLength) {
+					System.out.println(file.getPath() + " has an invalid resolution of " +
+							pattern.getWidth() + "x" + pattern.getHeight() + " pixels, and is being skipped.");
+					return;
+				}
 				for (int y = 0; y < pattern.getHeight(); y++) {
 					for (int x = 0; x < pattern.getWidth(); x++) {
 						long argbValue = pattern.getRGB(x, y);
@@ -126,20 +134,20 @@ public class AnimationPreviewer {
 						long blueValue = argbValue % 256;
 						if (alphaValue != 255 || redValue != greenValue || redValue != blueValue ||
 								redValue % grayscaleAmount != 0) {
-							System.out.println("Error: " + file.getPath() + " has an invalid " +
-									"ARGB value at (" + x + ", " + y + "): [" + alphaValue +
-									", " + redValue + ", " + greenValue + ", " + blueValue + "].");
+							System.out.println(file.getPath() + " has an invalid ARGB value at (" + x + ", " + y +
+									"): [" + alphaValue + ", " + redValue + ", " + greenValue + ", " + blueValue +
+									"], and is being skipped.");
 							return;
 						}
 					}
 				}
 				patterns.add(pattern);
-			} else {
-				System.out.println("Error: " + file.getPath() + " has an invalid resolution of " +
-						pattern.getWidth() + "x" + pattern.getHeight() + " pixels.");
+			} else if (file.isDirectory()) {
+				addPatternsFromDirectory(file);
 			}
-		} catch (Exception e) {
-			System.out.println("Error: " + file.getPath() + " does not contain a readable pattern.");
+			if (patterns.size() == MAXIMUM_LIST_SIZE) {
+				break;
+			}
 		}
 	}
 	
